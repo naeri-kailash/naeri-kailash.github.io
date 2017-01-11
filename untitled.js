23eac4bac47cfefab7b4fdda10da3090e126ee72
Naeri Kailash <naerikailash@Naeris-MacBook-Pro.local>
2017-01-12 11:36:03 +1300

    add mindfulness blog

---
+++ untitled.js
@@ -0,0 +1,12 @@
+function convertToF(celsius) {
+  var fahrenheit;
+  // Only change code below this line
+  var celcius;
+  fahrenheit = (celcius * (9/5) + 32);
+
+  // Only change code above this line
+  return fahrenheit;
+}
+
+// Change the inputs below to test your code
+convertToF(30);
